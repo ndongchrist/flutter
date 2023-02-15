3982a5f21ed00556218820bee8ef17bdf9d02e57
Akashiutchiha <christianhonore2003@gmail.com>
2023-02-15 14:23:26 +0100

    feat:updated this file

--- destini/lib/story.dart
+++ destini/lib/story.dart
@@ -6,9 +6,6 @@ class Story {
   String choice2 = "";
 
 //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-  Story({String storyTitle = "", String choice1 = "", String choice2 = ""}) {
-    storyTitle = storyTitle;
-    choice1 = choice1;
-    choice2 = choice2;
-  }
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
