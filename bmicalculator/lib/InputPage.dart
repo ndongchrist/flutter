70963f9c3f71896d3faaa9546ab705745e8a43ec
Akashiutchiha <christianhonore2003@gmail.com>
2023-02-15 23:13:56 +0100

    feat:Added Android App structure

---
+++ bmicalculator/lib/InputPage.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                ReusableCard(
+                  colour: Color(0xFF1D1A2F),
+                  cardChild: Column(
+                    children: [Icon(FontAwesomeIcons.mars), Text("Male")],
+                  ),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1A2F),
+                  cardChild: Column(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        color: Colors.amber,
+                      ),
+                      Text("Male")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                ReusableCard(
+                  colour: Color(0xFF1D1A2F),
+                  cardChild: Column(
+                    children: [Icon(FontAwesomeIcons.mars), Text("Male")],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                ReusableCard(
+                  colour: Color(0xFF1D1A2F),
+                  cardChild: Column(
+                    children: [Icon(FontAwesomeIcons.mars), Text("Male")],
+                  ),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1A2F),
+                  cardChild: Column(
+                    children: [Icon(FontAwesomeIcons.mars), Text("Male")],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              color: Color(0xFFD8004C),
+              height: 40,
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 0),
+            )
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour, required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 185,
+        margin: EdgeInsets.all(7.0),
+        decoration: BoxDecoration(
+            color: colour, borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+  }
+}
